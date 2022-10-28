a31a4ef255e2ceb9d3da6993ecdb347d5c006a0b
Moose17 <63780114+Mooose17@users.noreply.github.com>
2022-10-28 16:02:41 +0100

    adds routing and pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import './App.css';
-import Header from './Components/Header'
-import GetPokemonData from './Components/GetPokemonData'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./Pages/Home";
 
 function App() {
-
   return (
-    <div className="App">
-      <Header/>
-      <GetPokemonData/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
