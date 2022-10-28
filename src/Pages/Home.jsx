4479b1d21956aaf1c8d8d8a3de6cd6d8b0a590f7
Moose17 <63780114+Mooose17@users.noreply.github.com>
2022-10-28 16:04:03 +0100

    creates page for routing

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,17 @@
+import Header from "../Components/Header";
+import SearchPokemonById from "../Components/SearchPokemonById";
+
+const Search = () => {
+  return (
+    <>
+      <div>
+        <Header />
+      </div>
+      <div>
+        <SearchPokemonById />
+      </div>
+    </>
+  );
+};
+
+export default Search;
