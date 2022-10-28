ce42de0a29d59aaa5a71541bb73cfa50f2f63902
Moose17 <63780114+Mooose17@users.noreply.github.com>
2022-10-28 14:01:08 +0100

    queries pkmn-gen-1 collection in Firebase by ID

--- src/Components/SearchPokemonById.jsx
+++ src/Components/SearchPokemonById.jsx
@@ -1,3 +1,41 @@
+import { useState } from "react";
+import db from "../firebase";
+import { collection, query, where, getDocs } from "firebase/firestore";
+
 const SearchPokemonById = () => {
-    
-}
+  const [userInput, setUserInput] = useState("");
+  const pokemonRef = collection(db, "pokemon-kanto-gen-1");
+
+  const fetchQueryData = async () => {
+    const idQuery = query(pokemonRef, where("id", "==", parseInt(userInput)));
+    const querySnapshot = await getDocs(idQuery);
+    console.log(querySnapshot);
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, " => ", doc.data());
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetchQueryData();
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Poké ID:
+          <input
+            type="text"
+            name="name"
+            value={userInput}
+            onChange={(event) => setUserInput(event.target.value)}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+};
+
+export default SearchPokemonById;
